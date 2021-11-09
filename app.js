8a88ee1dda2300e759e9151c75a70ab251b362bf
avnishkumar dwivedi <avnishkumar.dwivedi@signiance.com>
2021-11-09 17:24:05 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello kishan</h1>');
+  res.send('<h1>Hello kishan and avish</h1>');
 })
 
 app.get('/products', (req, res) => {
